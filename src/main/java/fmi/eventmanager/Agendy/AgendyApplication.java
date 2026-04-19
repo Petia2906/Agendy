49cecbab918ce0bc14bb8dd1cf8bf4c1c307b009
Petya Hrusanova <petia.hrusanova@gmail.com>
2026-04-19 16:40:22 +0300

    initial commit

---
+++ src/main/java/fmi/eventmanager/Agendy/AgendyApplication.java
@@ -0,0 +1,13 @@
+package fmi.eventmanager.Agendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fmi/eventmanager/Agendy/AgendyApplicationTests.java
@@ -0,0 +1,13 @@
+package fmi.eventmanager.Agendy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
