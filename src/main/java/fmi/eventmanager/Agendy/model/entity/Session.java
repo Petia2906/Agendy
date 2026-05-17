dea91bba1a98be94e52d2d043554ce35dfd60244
polinizal <polina.staneva@yahoo.com>
2026-05-17 20:44:41 +0300

    add Hall, Event and Session entities

---
+++ src/main/java/fmi/eventmanager/Agendy/model/entity/Session.java
@@ -0,0 +1,57 @@
+package fmi.eventmanager.Agendy.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "session")
+@Getter
+@Setter
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "speaker_id")
+    private User speaker; //ask if there is Speaker entity
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hall_id", nullable = false)
+    private Hall hall;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    public Session() {} //check if should be protected
+
+    public Session(Event event, User speaker, Hall hall, String title, String description, LocalDateTime startTime, LocalDateTime endTime) {
+        this.event = event;
+        this.speaker = speaker;
+        this.hall = hall;
+        this.title = title;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+   //with @Getter and @Setter Lombok takes care of the getters and setters
+
+}
