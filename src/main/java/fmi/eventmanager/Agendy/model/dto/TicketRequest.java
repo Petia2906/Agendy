8f3efb2782a03effaca3efdb860d9468e36c18fa
polinizal <polina.staneva@yahoo.com>
2026-06-25 11:08:03 +0300

    bug fix - ticket price is now coming from event and not request lol

--- src/main/java/fmi/eventmanager/Agendy/model/dto/TicketRequest.java
+++ src/main/java/fmi/eventmanager/Agendy/model/dto/TicketRequest.java
@@ -4,13 +4,10 @@ import fmi.eventmanager.Agendy.model.entity.TicketType;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-
 @Getter
 @Setter
 public class TicketRequest {
     private TicketType ticketType;
-    private BigDecimal price;
 
     public TicketRequest() { }
 }
