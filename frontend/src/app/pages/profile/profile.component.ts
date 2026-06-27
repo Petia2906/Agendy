9e79f8b807f57ae5cdc12d6e6e42de0b4e5e80fe
polinizal <polina.staneva@yahoo.com>
2026-06-27 10:18:24 +0300

    add personal profile page for all users

---
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { AuthService, CurrentUser } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-profile',
+  imports: [CommonModule, HeaderComponent],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent implements OnInit {
+  user: CurrentUser | null = null;
+  loading = true;
+  error = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.getCurrentUser().subscribe({
+      next: (data) => {
+        this.user = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load profile.';
+        this.loading = false;
+      }
+    });
+  }
+}
